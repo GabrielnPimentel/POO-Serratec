a94cdc878c8802cc6d271fa72afb3f85d90b08fb
GabrielNPimentel <gabrielnpimentel@hotmail.com>
2024-04-09 23:17:24 -0300

    Adição do Application e do pacote Util

---
+++ individual/src/br/com/poo/individual/application/IndividualApplication.java
@@ -0,0 +1,14 @@
+package br.com.poo.individual.application;
+
+import br.com.poo.individual.io.RelatorioIO;
+
+public class IndividualApplication {
+
+	public static void main(String[] args) {
+		
+		RelatorioIO.leitor("banco");
+		RelatorioIO.escritor("banco");
+
+	}
+
+}
